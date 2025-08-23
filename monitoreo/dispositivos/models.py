278e782bbe9d3e686193ad0684575683320f15b7
felipe jimenez <felipe.jl.0209@gmail.com>
2025-08-23 15:17:33 -0400

    categorias de los modelos listas

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -8,13 +8,38 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class Zona(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
 
 class Dispositivo(models.Model):
     nombre = models.CharField(max_length=100)
     consumo = models.FloatField()
     estado = models.BooleanField(default=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    zona = models.ForeignKey(Zona, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nombre
     
+class Medicion(models.Model):
+    dispositivo = models.ForeignKey(Dispositivo, on_delete=models.CASCADE, related_name="mediciones")
+    fecha_hora = models.DateTimeField(auto_now_add=True)
+    consumo = models.FloatField(help_text="Consumo registrado")
+
+    def __str__(self):
+        return f"{self.dispositivo} - {self.fecha_hora} - Consumo:{self.consumo} "
+    
+class Alerta(models.Model):
+    dispositivo = models.ForeignKey(Dispositivo, on_delete=models.CASCADE)
+    mensaje = models.TextField()
+    fecha_hora = models.DateTimeField(auto_now_add=True)
+    consumo_detectado = models.FloatField(help_text="Consumo detectado")
+
+    
+
 
