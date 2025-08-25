35c74e5115c49ddafc323747c7df8a3045727714
felipe jimenez <felipe.jl.0209@gmail.com>
2025-08-25 13:28:04 -0400

    Agregacion de la clase baseModel

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -2,21 +2,35 @@ from django.db import models #los imports
 
 # Create your models here.
 
-class Categoria(models.Model):
+class BaseModel(models.Model):
+    ESTADOS = [
+        ("ACTIVO", "Activo"),
+        ("INACTIVO", "Inactivo"),
+    ]
+
+    estado = models.CharField(max_length=10, choices=ESTADOS, default="ACTIVO")
+    created_at = models.DateTimeField(auto_now_add=True)  # se asigna al crear
+    updated_at = models.DateTimeField(auto_now=True)  # se actualiza cada vez que se guarda
+    deleted_at = models.DateTimeField(null=True, blank=True)  # opcional para borrado lógico
+
+    class Meta:
+        abstract = True  # no crea tabla, solo se hereda
+
+class Categoria(BaseModel):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nombre
     
-class Zona(models.Model):
+class Zona(BaseModel):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nombre
 
-class Dispositivo(models.Model):
+class Dispositivo(BaseModel):
     nombre = models.CharField(max_length=100)
     consumo = models.FloatField()
     estado = models.BooleanField(default=True)
@@ -26,7 +40,7 @@ class Dispositivo(models.Model):
     def __str__(self):
         return self.nombre
     
-class Medicion(models.Model):
+class Medicion(BaseModel):
     dispositivo = models.ForeignKey(Dispositivo, on_delete=models.CASCADE, related_name="mediciones")
     fecha_hora = models.DateTimeField(auto_now_add=True)
     consumo = models.FloatField(help_text="Consumo registrado")
@@ -34,7 +48,7 @@ class Medicion(models.Model):
     def __str__(self):
         return f"{self.dispositivo} - {self.fecha_hora} - Consumo:{self.consumo} "
     
-class Alerta(models.Model):
+class Alerta(BaseModel):
     dispositivo = models.ForeignKey(Dispositivo, on_delete=models.CASCADE)
     mensaje = models.TextField()
     fecha_hora = models.DateTimeField(auto_now_add=True)
