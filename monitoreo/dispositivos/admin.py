9df45953396b2f48e905bdea82fb97ed4c3cbe5e
felipe jimenez <felipe.jl.0209@gmail.com>
2025-08-25 14:00:20 -0400

    clase 5 completada, superuser y insercion por tempale

--- monitoreo/dispositivos/admin.py
+++ monitoreo/dispositivos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria, Zona, Dispositivo, Medicion, Alerta
+
+@admin.register(Dispositivo)
+class DispositivoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'consumo', 'estado', 'categoria', 'zona')
+    list_filter = ('categoria', 'estado')
+    search_fields = ('nombre', 'consumo')
