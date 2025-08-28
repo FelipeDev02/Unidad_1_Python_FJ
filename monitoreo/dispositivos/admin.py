2d4a7643e6ebc264a6cfcdf19bba1f6a6b388c80
felipe jimenez <felipe.jl.0209@gmail.com>
2025-08-28 14:15:20 -0400

    modificacion models para mostrar todas las tablas para insertar

--- monitoreo/dispositivos/admin.py
+++ monitoreo/dispositivos/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Categoria, Zona, Dispositivo, Medicion, Alerta
 
+admin.site.register([Categoria, Zona, Medicion, Alerta])
+
 @admin.register(Dispositivo)
 class DispositivoAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'consumo', 'estado', 'categoria', 'zona')
